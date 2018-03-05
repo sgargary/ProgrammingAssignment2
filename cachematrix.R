586389e25cb8d7697665640f1bf62732d9175611
sgargary <36457140+sgargary@users.noreply.github.com>
2018-03-05 15:31:37 -0700

    https://github.com/sgargary/ProgrammingAssignment2/edit/master/cachematrix.R
    This assignment enhanced my skills by calculating the inverse of matrix. This is a very crucial also time consuming process. Using the two steps in computing the inverse of matrix suggested in here was highly efficient.
    7f657dd22ac20d22698c53b23f0057e1a12c09b7

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## functions do: Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## Write a short comment describing this function: create a matrix that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        invr <- NULL
+  set <- function(y) {
+    x <<- y
+    invr <<- NULL
 
 }
 
 
-## Write a short comment describing this function
+## Write a short comment describing this function: this function solves for inverse of the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if(!is.null(invr)) {
+    message("Getting Cached Data:-")
+    return(invr)
+  }
+  matrx <- x$get()
+  invr <- solve(matrx, ...)
+  x$setinverse(invr)
+  invr
 }
